ed419e5dda678fce98a3a42e0edf6c66b2c3ac4a
Yitao Xiong <hb1412@gmail.com>
2016-02-27 14:21:14 -0500

    Finally get everything working!

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 var path = require('path');
-
-module.exports = {
-    entry: [
-      'webpack/hot/dev-server',
-      'webpack-dev-server/client?http://localhost:8080',
-      path.resolve(__dirname, 'app/main.js')
-    ],
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
+var config = {
+  entry: path.resolve(__dirname, 'app/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
 };
+
+module.exports = config;
