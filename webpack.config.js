854d7c789f490ae8aacc91c3dc38e135421dd118
Yitao Xiong <yitao@bitsighttech.com>
2016-06-07 15:03:02 -0400

    Added js to use babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,14 @@ var config = {
         }
       },
       {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
         test: /\.css$/, 
         loader: "style-loader!css-loader"
       }
