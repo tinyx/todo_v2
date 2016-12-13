fa39170b50d5d10878dbc00f124c504f26c76522
Yitao Xiong <yitao@bitsighttech.com>
2016-12-13 14:36:34 -0500

    Save

--- src/lib/actions/authentication.js
+++ src/lib/actions/authentication.js
@@ -4,7 +4,7 @@ import jwtDecode from 'jwt-decode';
 import { push } from 'react-router-redux'
 
 function delete_cookie(name) {
-  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=.crabfactory.net';
 }
 
 export function loginUserSuccess(token) {
